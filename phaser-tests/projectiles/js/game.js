d88acb1f0ac1a8e67d04717f848372f037493898
AlexVanderbist <AlexVanderbist@gmail.com>
2016-03-07 15:19:30 +0100

    working angry tacos

--- phaser-tests/projectiles/js/game.js
+++ phaser-tests/projectiles/js/game.js
@@ -23,6 +23,7 @@ preload.prototype = {
         // Preload images
         game.load.image("trump", "assets/trump.png");
         game.load.image("bodyguard", "assets/bodyguard.png");
+        game.load.image("taco", "assets/taco.png");
         
     },
     create: function () {
@@ -36,20 +37,41 @@ preload.prototype = {
 playgame = function(game) {};
 playgame.prototype = {
     create: function () {
+
+        game.physics.startSystem(Phaser.Physics.P2JS);
+
+        projectiles = game.add.group();
+        projectiles.enableBody = true;
+        projectiles.physicsBodyType = Phaser.Physics.P2JS;
       
         keyW = game.input.keyboard.addKey(Phaser.Keyboard.W);
         keyW.onDown.add(this.addProjectile, this);
+
+        game.trump = game.add.sprite(game.world.centerX, game.world.centerY, 'trump');
+        game.trump.anchor.setTo(0.5,0.5);
         
     },
     update: function () {
 
+        game.trump.angle += 1;
+
+        projectiles.forEachAlive(this.moveProjectile,this);
 
     },
     addProjectile: function () {
-        var water = game.add.sprite(game.world.randomX, game.world.randomY, 'water');
-        water.animations.add('waterDrop');
-        water.animations.play('waterDrop', 30);
+        var taco = game.add.sprite(game.world.randomX, game.world.randomY, 'taco');
+        projectiles.add(taco);
         // var sound = game.add.audio('drop');
         // sound.play();
+    },
+    moveProjectile: function (projectile) {
+        this.accelerateToObject(projectile,game.trump,30);
+    },
+    accelerateToObject: function (obj1, obj2, speed) {
+        if (typeof speed === 'undefined') { speed = 60; }
+        var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
+        obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
+        obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject 
+        obj1.body.force.y = Math.sin(angle) * speed;
     }
 }
