8a69eb319e6b6e3e3e45e9393423ca2b531813a6
AlexVanderbist <alex.vanderbist@gmail.com>
2016-03-09 15:37:27 +0100

    added tap to start text

--- 00-final-project/www/js/states/MainMenu.js
+++ 00-final-project/www/js/states/MainMenu.js
@@ -2,6 +2,7 @@ Trump.MainMenu = function (game) {
 
 	this.playButton = null;
 	this.menuBackground = null;
+	this.tapToPlayText = null;
 
 };
 
@@ -12,6 +13,10 @@ Trump.MainMenu.prototype = {
 		this.menuBackground = this.add.sprite(0,0, 'menuBackground');
 		this.menuBackground.scale.setTo(0.3333);
 
+		var style = {font: "40px Arial", fill: "#ffffff", align: "center"};
+		this.labelGuards = this.add.text(this.world.centerX, this.world.height - 300, "Tap to start!", style);
+		this.labelGuards.anchor.set(0.5);
+		
 		//this.playButton = this.add.button(400, 600, 'addingGuard', this.startGame, this, 'buttonOver', 'buttonOut', 'buttonOver');
 
         this.input.onDown.addOnce(this.startGame, this);
