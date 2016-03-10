7bfdeb0c5e71dc6a81a24713bdf81c39e72e9e33
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-03-10 11:29:35 +0100

    Different color guard

---
+++ 10-phaser-tests/guardColor/www/js/main.js
@@ -0,0 +1,20 @@
+var game;
+
+function loadGame ()
+{
+
+	// Create a new Phaser Game
+	game = new Phaser.Game(480, 800);
+
+	// Add the game states
+	game.state.add("Boot", Trump.Boot);
+	game.state.add("Preloader", Trump.Preloader);
+	game.state.add("MainMenu", Trump.MainMenu);
+	game.state.add("Game", Trump.Game);
+
+	// Start the "Boot" state
+	game.state.start("Boot");
+};
+
+loadGame();
+//document.addEventListener('deviceready', loadGame, false);
