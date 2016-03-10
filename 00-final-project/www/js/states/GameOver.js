9e1fc341076a6511a74b0d13c7260ac9249968db
rubends <rubendeswaef@gmail.com>
2016-03-10 15:03:26 +0100

    fixed scores

--- 00-final-project/www/js/states/GameOver.js
+++ 00-final-project/www/js/states/GameOver.js
@@ -11,7 +11,18 @@ Trump.GameOver.prototype = {
 		this.menuBackground = this.add.sprite(0,0, 'menuBackground');
 		this.menuBackground.scale.setTo(0.3333);
 
-        this.playButton = game.add.button(game.world.centerX, game.world.centerY + 180, 'playButton', this.startGame, this, 0, 0, 1);
+		var overstyle = {font: "50px Arial", fill: "#000000", align: "center"};
+		this.labelOver = this.add.text(this.world.centerX, 90, "GAME OVER", overstyle); 
+		this.labelOver.anchor.set(0.5);
+
+		var scorestyle = {font: "40px Arial", fill: "#ffffff", align: "center"};
+		this.labelShowScore = this.add.text(this.world.centerX, this.world.centerY + 135, "SCORE\n" + game.score, scorestyle); 
+		this.labelShowScore.anchor.set(0.5);
+
+		this.labelShowScore = this.add.text(this.world.centerX, this.world.centerY + 235, "HIGHSCORE\n" + game.bestScore, scorestyle); 
+		this.labelShowScore.anchor.set(0.5);
+
+        this.playButton = this.add.button(this.world.centerX, this.world.height - 60, 'playButton', this.startGame, this, 0, 0, 1);
         this.playButton.scale.setTo(0.3333);
         this.playButton.anchor.setTo(0.5);
 
